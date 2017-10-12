089863685a0a77e3810f1633a7aa9c319cf4186c
Joyce Yang <jcyang@hmc.edu>
2017-10-12 15:00:53 -0400

    Included check with while loop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,37 +13,39 @@ public class Main {
         double taxPercent=0.0;
         double taxAmount=0.0;
         String taxCode="";
+        String loopSwitch="";
         NumberFormat nf = NumberFormat.getInstance();
         nf.setMaximumFractionDigits(2);
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the customer ID: ");
-        custId=scan.nextInt();
-        System.out.println("Enter the name: ")    ;
-        custName=scan.next();
-        lineBreak=scan.nextLine();
-        System.out.println("Enter the sales amount: ");
-        salesAmount=scan.nextDouble();
-        System.out.println("Enter the tax code: ");
-        taxCode=scan.next();
-        lineBreak=scan.nextLine();
-        if (taxCode.equals("NRM")){
-         taxPercent=0.06;
-        }
-        else if (taxCode.equals("NPF")){
-            taxPercent=0.00;
-        }
-        else if (taxCode.equals("BIZ")){
-            taxPercent=0.045;
-        }
-        taxAmount=calculateTax(taxPercent, salesAmount);
-        double unformattedAmount=taxAmount+salesAmount;
+        while(!loopSwitch.equalsIgnoreCase("n")) {
+            System.out.println("Enter the customer ID: ");
+            custId = scan.nextInt();
+            System.out.println("Enter the name: ");
+            custName = scan.next();
+            lineBreak = scan.nextLine();
+            System.out.println("Enter the sales amount: ");
+            salesAmount = scan.nextDouble();
+            System.out.println("Enter the tax code: ");
+            taxCode = scan.next();
+            lineBreak = scan.nextLine();
+            if (taxCode.equals("NRM")) {
+                taxPercent = 0.06;
+            } else if (taxCode.equals("NPF")) {
+                taxPercent = 0.00;
+            } else if (taxCode.equals("BIZ")) {
+                taxPercent = 0.045;
+            }
+            taxAmount = calculateTax(taxPercent, salesAmount);
+            double unformattedAmount = taxAmount + salesAmount;
 
-        System.out.println("Customer ID: "+ custId);
-        System.out.println("Customer Name: "+custName);
-        System.out.println("Sales Amount: "+salesAmount);
-        System.out.println("Tax Code: "+taxCode);
-        System.out.println("Total Amount Due: "+ nf.format(unformattedAmount));
-
+            System.out.println("Customer ID: " + custId);
+            System.out.println("Customer Name: " + custName);
+            System.out.println("Sales Amount: " + salesAmount);
+            System.out.println("Tax Code: " + taxCode);
+            System.out.println("Total Amount Due: " + nf.format(unformattedAmount));
+            System.out.println("Do you want to enter a new record? (Y/N) ");
+                    loopSwitch=scan.nextLine();
+        }
 
     }
 
